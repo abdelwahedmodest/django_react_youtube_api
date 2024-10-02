9fab63545cf4cd9a9f8023fb65f637681fa935b0
abdelwahed <abdelwahedmodest@gmail.com>
2024-10-02 06:40:30 -0700

    add  links   in   navbar

--- frontend/reactfront/src/App.js
+++ frontend/reactfront/src/App.js
@@ -1,59 +1,51 @@
 import React from 'react';
-import  Login  from  "./Login"
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Login from "./Login";
 import InstagramLogin from './Logininsta';
-//import { useEffect, useState } from "react";
-//import  Im  from  "./Im"
-import  SData  from  "./SData"
-import  {BrowserRouter,Routes,Route}  from  "react-router-dom"
-import  UploadForm   from  "./Formreact"
-//import  {Router,Route,Routes}  from  "react-router-dom";
-//{import { Link } from 'react-router-dom';
-import  Home  from  "./Home";
+import SData from "./SData";
+import UploadForm from "./Formreact";
+import Home from "./Home";
 
 function App() {
-  //const  [mohamed,setmohamed]=useState(0)
-  //const  [number,setnumber]=useState("")
-/* const plushandler=()=>{
-  setmohamed(mohamed + 1)
- }
- const minushandler=()=>{
-  setmohamed(mohamed - 1)
- }*/
-
   return (
-    <>
-    {/* <Login />
-    <SData />
-    <UploadForm />
-    <Home />
-    <InstagramLogin />
-  <Router>
-      <nav>
-        <ul>
-          <li><Link to="/">Home</Link></li>
-          <li><Link to="/login">Login</Link></li>
-          <li><Link to="/data">Afficher les données</Link></li>
-        </ul>
-      </nav>
-  <button  onClick={plushandler}>+</button>
-   {mohamed}
-   <button  onClick={minushandler}>+</button>
-   <button  onClick={()=>{setnumber(number*2)}}>INCREMENT</button>
-    {number}
-    Définir les différentes routes */}
     <BrowserRouter>
-    <Routes>
+      {/* Navbar Bootstrap */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <Link className="navbar-brand" to="/">MyApp</Link>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className="nav-link" to="/">Home</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/login">Login</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/data">Afficher les données</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/formdata">Formulaire de téléchargement</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/insta">Login Instagram</Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      {/* Routes de l'application */}
+      <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/data" element={<SData />} />
         <Route path="/formdata" element={<UploadForm />} />
-        <Route  path="/insta"  element={<InstagramLogin />} />
+        <Route path="/insta" element={<InstagramLogin />} />
       </Routes>
     </BrowserRouter>
-
-  </>   
-    );
-  }
-
+  );
+}
 
 export default App;
