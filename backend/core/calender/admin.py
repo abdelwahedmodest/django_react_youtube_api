0a4cb84f305ddecc804a55668a030e81b5b491bc
abdelwahed <abdelwahedmodest@gmail.com>
2024-10-31 02:15:25 -0700

    Keep only the frontend folder in frontend-branch

--- backend/core/calender/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import UserActivity
-
-@admin.register(UserActivity)
-class UserActivityAdmin(admin.ModelAdmin):
-    list_display = ('user', 'selected_date', 'activity_details')
-    search_fields =('user', 'selected_date', 'activity_details') 
-    list_filter = ('user',)
--- backend/core/contactus/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import upload_media
-
-urlpatterns = [
-    path('api/upload/', upload_media, name='upload_media'),
-]
